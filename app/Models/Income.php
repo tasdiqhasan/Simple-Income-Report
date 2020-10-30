f5146057a712b8ae2ec91ecc5417b65aa2f2c872
Tasdiq Hasan <hasantdq@gmail.com>
2020-10-30 16:38:02 +0700

    fix typo

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -9,7 +9,7 @@ class Income extends Model
 {
     use SoftDeletes;
 
-    protected $filable = ['type', 'description', 'amount', 'currency'];
+    protected $fillable = ['type', 'description', 'amount', 'currency'];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
