009bd6b28d5101fff568dc7c6e0d7e487774a0e8
Tasdiq Hasan <hasantdq@gmail.com>
2020-10-30 10:12:07 +0700

    add incomes table migration

---
+++ database/migrations/2020_10_30_030728_create_incomes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('amount')->default(0);
+            $table->string('currency')->default('IDR');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
