4148703fbfab62feb7ecb5f4f8303f9a5d29e915
Tasdiq Hasan <hasantdq@gmail.com>
2020-10-30 10:42:02 +0700

    init api controllers

---
+++ app/Http/Controllers/Api/IncomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IncomeController extends Controller
+{
+    public function store(Request $request)
+    {
+        
+    }
+    
+    public function update(Request $request)
+    {
+
+    }
+    
+    public function delete(Request $request)
+    {
+
+    }
+}
