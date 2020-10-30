39f99f1e02cc9c7f9ae7ebe5ba3e2eed286a267c
Tasdiq Hasan <hasantdq@gmail.com>
2020-10-30 11:21:02 +0700

    init api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'incomes'], function() {
+    Route::get('/type', 'Api\IncomeTypeController@index');
+    Route::post('/create', 'Api\IncomeController@store');
+    Route::post('/{id}/edit', 'Api\IncomeController@update');
+    Route::post('/{id}/delete', 'Api\IncomeController@delete');
+});
+
